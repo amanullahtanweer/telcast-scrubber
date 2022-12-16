e50d9cb80546da18b095a21a18d384785af551b7
Amanullah Tanweer <inheritedarts@gmail.com>
2022-12-16 17:04:42 +0500

    manual scrub

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,7 +8,25 @@ class DashboardController < ApplicationController
   end
 
   def manual_result
-    @result = ""
+    numbers = params['numbers'] || []
+		numbers = numbers.split("\r\n")
+    numbers = numbers.map {|x| x.delete_prefix('1')}
+    numbers = numbers.reject {|x| x.length != 10}
+    @good_numbers = []
+    @bad_numbers  = []
+    if numbers.count > 0
+      master = $redis.SMEMBERS 'master'
+      found  = $redis.SMISMEMBER 'master', numbers
+    end
+
+    numbers.each_with_index do |row, index|
+      if found[index] == 1
+        @bad_numbers << row
+      else
+        @good_numbers << row
+      end
+    end
+
     render 'download'
   end
 
