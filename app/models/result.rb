3b4bb48d0a994449135cde027c529674c0b849b3
Amanullah Tanweer <inheritedarts@gmail.com>
2023-03-25 00:07:54 +0500

    updates

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,8 +1,8 @@
 class Result < ApplicationRecord
   belongs_to :user
-  has_one_attached :file
-  has_one_attached :good_file
-  has_one_attached :bad_file
+  has_one_attached :file, :dependent => :destroy
+  has_one_attached :good_file, :dependent => :destroy
+  has_one_attached :bad_file, :dependent => :destroy
 
   after_create_commit :start_scrubbing
   default_scope { order(created_at: :desc) }
